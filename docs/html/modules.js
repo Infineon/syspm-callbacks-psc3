8673566ba5e882b1426afff76032fbd627c921f7
gitlab-runner <do-not-reply@cypress.com>
2025-03-20 13:32:25 -0500

    Upload syspm-callbacks-psc3 [3365]

---
+++ docs/html/modules.js
@@ -0,0 +1,9 @@
+var modules =
+[
+    [ "DMA Deep Sleep Callback", "group__group__DMA.html", "group__group__DMA" ],
+    [ "SYSPM Callbacks Availability", "group__group__syspm__availability.html", "group__group__syspm__availability" ],
+    [ "NVM Deep Sleep Callback", "group__group__nvm.html", "group__group__nvm" ],
+    [ "PDL Aliases", "group__group__pdl__aliases.html", "group__group__pdl__aliases" ],
+    [ "SCB UART Deep Sleep Callback", "group__group__uart.html", "group__group__uart" ],
+    [ "TCPWM Deep Sleep Callback", "group__group__tcpwm.html", "group__group__tcpwm" ]
+];
---
+++ docs/html/search/groups_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['scb_20uart_20deep_20sleep_20callback_0',['SCB UART Deep Sleep Callback',['../group__group__uart.html',1,'']]],
+  ['syspm_20callbacks_20availability_1',['SYSPM Callbacks Availability',['../group__group__syspm__availability.html',1,'']]]
+];
